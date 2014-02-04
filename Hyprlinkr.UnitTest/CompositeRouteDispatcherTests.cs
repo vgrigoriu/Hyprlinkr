d335d11564cee48a9ee3fc7cb92a377798801840
Victor Grigoriu <vgrigoriu@gmail.com>
2014-02-05 00:50:30 +0200

    Add CompositeRouteDispatcher

---
+++ Hyprlinkr.UnitTest/CompositeRouteDispatcherTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Moq;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.Hyprlinkr.UnitTest
+{
+    public class CompositeRouteDispatcherTests
+    {
+        [Theory, AutoHypData]
+        public void SutHasAppropriateGuards(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(CompositeRouteDispatcher));
+        }
+
+        [Theory, AutoHypData]
+        public void SutReturnsRouplesFromInnerRouteDispatchers(
+            [Frozen]Mock<IRouteDispatcher> firstDispatcherStub,
+            [Frozen]Mock<IRouteDispatcher> secondDispatcherStub,
+            [Frozen]Rouple firstRouple,
+            [Frozen]Rouple seconRouple,
+            CompositeRouteDispatcher sut)
+        {
+            // Fixture setup
+            Expression<Action<CompositeRouteDispatcherTests>> exp =
+                c => c.SutHasAppropriateGuards(null);
+            var methodCallExp = (MethodCallExpression)exp.Body;
+
+            firstDispatcherStub
+                .Setup(d => d.Dispatch(
+                    It.IsAny<MethodCallExpression>(),
+                    It.IsAny<IDictionary<string, object>>()))
+                .Returns(new[] {firstRouple});
+            secondDispatcherStub
+                .Setup(d => d.Dispatch(
+                    It.IsAny<MethodCallExpression>(),
+                    It.IsAny<IDictionary<string, object>>()))
+                .Returns(new[] { seconRouple });
+
+            var actual = sut.Dispatch(methodCallExp, new Dictionary<string, object>());
+
+            Assert.Contains(firstRouple, actual);
+            Assert.Contains(seconRouple, actual);
+        }
+    }
+}
---
+++ Hyprlinkr/CompositeRouteDispatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Ploeh.Hyprlinkr
+{
+    /// <summary>
+    /// A composite of other <see cref="IRouteDispatcher"/>
+    /// </summary>
+    public class CompositeRouteDispatcher: IRouteDispatcher
+    {
+        private readonly IEnumerable<IRouteDispatcher> innerDispatchers;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositeRouteDispatcher" /> class.
+        /// </summary>
+        /// <param name="innerDispatchers">The collection of inner dispatchers</param>
+        public CompositeRouteDispatcher(IEnumerable<IRouteDispatcher> innerDispatchers)
+        {
+            if (innerDispatchers == null) throw new ArgumentNullException("innerDispatchers");
+            this.innerDispatchers = innerDispatchers;
+        }
+
+        /// <summary>
+        /// Provides dispatch information from all the inner dispatchers.
+        /// </summary>
+        /// <param name="method">The method expression.</param>
+        /// <param name="routeValues">Route values.</param>
+        /// <returns>All the <see cref="Rouple"/> instances returned by the inner route dispatchers.</returns>
+        public IEnumerable<Rouple> Dispatch(MethodCallExpression method, IDictionary<string, object> routeValues)
+        {
+            if (method == null) throw new ArgumentNullException("method");
+            if (routeValues == null) throw new ArgumentNullException("routeValues");
+
+            return innerDispatchers.SelectMany(inner => inner.Dispatch(method, routeValues));
+        }
+    }
+}
